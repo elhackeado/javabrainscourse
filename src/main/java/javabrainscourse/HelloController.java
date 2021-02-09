0a582f793c9ba24a68819fe925e67266e201f495
aman.thakur0001 <aman.thakur0001@hotmail.com>
2021-02-09 07:59:32 +0530

    first commit

---
+++ src/main/java/javabrainscourse/HelloController.java
@@ -0,0 +1,14 @@
+package javabrainscourse;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+	@RequestMapping("/")
+	public String sayHi() {
+		return "Welcome to JavaBrains Course API";
+	}
+
+}
